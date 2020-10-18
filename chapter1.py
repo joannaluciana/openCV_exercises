9414ae527d68f7413053aa5dd5430ebeaca4d1df
Joanna <joanna@wloskowicz.pl>
2020-10-18 22:17:44 +0200

    adding lines

--- chapter1.py
+++ chapter1.py
@@ -1,13 +1,11 @@
 
 import cv2
 import numpy as np
-img = cv2.imread("Resources/portret_small.jpg")
-print(img.shape)
-imgResize = cv2.resize(img,(600,500))
 
-imgCroped = img[0:200,200:500]
-
-cv2.imshow("image",img)
-cv2.imshow("image resize",imgResize)
-cv2.imshow("image croped",imgCroped)
+img = np.zeros((512,512,3),np.uint8)
+print(img)
+# img[200:500,200:500]=255,0,0
+# matrix size
+cv2.line(img,(0,0),(300,300),(0,255,0),4)
+cv2.imshow("image", img)
 cv2.waitKey(0)
