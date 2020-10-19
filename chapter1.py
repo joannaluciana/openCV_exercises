988e1e32365c5af1b8a4bc01cd91d17a26a1ad96
Joanna <joanna@wloskowicz.pl>
2020-10-19 18:10:14 +0200

    adding pink line to figures and reafctoring of code

--- chapter1.py
+++ chapter1.py
@@ -4,19 +4,28 @@ import numpy as np
 from stack import stackImages
 from getCont import getContours
 
+# def getContours(img):
+
+#     contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+#     for cnt in contours:
+#         area = cv2.contourArea(cnt)
+#         print(area)
+#         cv2.drawContours(imgContour, cnt, -1, (255,153,255),10)
+
 path = 'Resources/geom.png'
 img = cv2.imread(path)
+imgContour = img.copy()
 
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 imgBlur = cv2.GaussianBlur(imgGray,(7,7),1)
 imgCanny=cv2.Canny(imgBlur,50,50)
-getContours(imgCanny)
+getContours(imgCanny,imgContour)
 
 imgBlank=np.zeros_like(img)
 
 
 imgStack = stackImages(0.6,([img,imgGray,imgBlur],
-[img,imgGray,imgBlank]))
+[img,imgContour,imgContour]))
 
 cv2.imshow("Stacked ",imgStack)
 
--- getCont.py
+++ getCont.py
@@ -1,9 +1,12 @@
 import cv2
 import numpy as np
 
-def getContours(img):
+def getContours(img,imgCon):
+
+    imgContour = img.copy()
 
     contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     for cnt in contours:
         area = cv2.contourArea(cnt)
         print(area)
+        cv2.drawContours(imgCon, cnt, -1, (255,153,255),10)
