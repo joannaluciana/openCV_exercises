595c7cb1bc0445245a0b271ea2c428661bcd8356
Joanna <joanna@wloskowicz.pl>
2020-10-19 17:49:51 +0200

    adding area calculating function

--- chapter1.py
+++ chapter1.py
@@ -2,13 +2,21 @@
 import cv2
 import numpy as np
 from stack import stackImages
+from getCont import getContours
 
 path = 'Resources/geom.png'
 img = cv2.imread(path)
 
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 imgBlur = cv2.GaussianBlur(imgGray,(7,7),1)
-imgStack = stackImages(0.6,([img,imgGray,imgBlur]))
+imgCanny=cv2.Canny(imgBlur,50,50)
+getContours(imgCanny)
+
+imgBlank=np.zeros_like(img)
+
+
+imgStack = stackImages(0.6,([img,imgGray,imgBlur],
+[img,imgGray,imgBlank]))
 
 cv2.imshow("Stacked ",imgStack)
 
---
+++ getCont.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+
+def getContours(img):
+
+    contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        print(area)
