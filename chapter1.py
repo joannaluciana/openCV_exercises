d5cfe84eaaafa953fa6d4c39e1958a2103c4a3be
Joanna <joanna@wloskowicz.pl>
2020-10-19 15:15:15 +0200

    code refactoring and adding fig gif

--- chapter1.py
+++ chapter1.py
@@ -1,72 +1,15 @@
 
 import cv2
 import numpy as np
-def empty(a):
-    pass
+from stack import stackImages
 
-def stackImages(scale,imgArray):
-    rows = len(imgArray)
-    cols = len(imgArray[0])
-    rowsAvailable = isinstance(imgArray[0], list)
-    width = imgArray[0][0].shape[1]
-    height = imgArray[0][0].shape[0]
-    if rowsAvailable:
-        for x in range ( 0, rows):
-            for y in range(0, cols):
-                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
-                else:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
-                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
-        imageBlank = np.zeros((height, width, 3), np.uint8)
-        hor = [imageBlank]*rows
-        hor_con = [imageBlank]*rows
-        for x in range(0, rows):
-            hor[x] = np.hstack(imgArray[x])
-        ver = np.vstack(hor)
-    else:
-        for x in range(0, rows):
-            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
-                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
-            else:
-                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
-            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
-        hor= np.hstack(imgArray)
-        ver = hor
-    return ver
+path = 'Resources/geom.png'
+img = cv2.imread(path)
 
-path = 'Resources/cards.jpg'
-cv2.namedWindow("Trackbars")
-cv2.resizeWindow("Trackbars",640,240)
-h_min = cv2.createTrackbar("Hue min","Trackbars", 0, 144,empty)
-h_max = cv2.createTrackbar("Hue max","Trackbars", 144, 144,empty)
-s_min = cv2.createTrackbar("Sat min","Trackbars", 0, 255,empty)
-s_max = cv2.createTrackbar("Sat max","Trackbars", 255, 255,empty)
-v_min = cv2.createTrackbar("Val min","Trackbars", 0, 166,empty)
-v_max = cv2.createTrackbar("Val max","Trackbars", 166, 166,empty)
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+imgBlur = cv2.GaussianBlur(imgGray,(7,7),1)
+imgStack = stackImages(0.6,([img,imgGray,imgBlur]))
 
+cv2.imshow("Stacked ",imgStack)
 
-while True:
-    img=cv2.imread(path)
-    imgHSV= cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-    h_min = cv2.getTrackbarPos("Hue min","Trackbars")
-    h_max = cv2.getTrackbarPos("Hue max","Trackbars")
-    s_min = cv2.getTrackbarPos("Sat min","Trackbars")
-    s_max = cv2.getTrackbarPos("Sat max","Trackbars")
-    v_min = cv2.getTrackbarPos("Val min","Trackbars")
-    v_max = cv2.getTrackbarPos("Val max","Trackbars")
-    print(h_min,h_max,s_min,s_max,v_min,v_max)
-
-    lower=np.array([h_min,s_min,v_min])
-    upper=np.array([h_max,s_max,v_max])
-
-    mask = cv2.inRange(imgHSV,lower,upper)
-
-    imgResult = cv2.bitwise_and(img,img,mask=mask)
-    imgStack = stackImages(0.6,([img,imgHSV],[mask,imgResult]))
-#  two img in one
-
-    cv2.imshow("Stacked images",imgStack)
-
-
-    cv2.waitKey(1)
+cv2.waitKey(0)
